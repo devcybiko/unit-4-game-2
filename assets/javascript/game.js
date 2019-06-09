f87e9b973396b3499dd64abf98605b4994e3e6df
2mlcmonkeys <2mlcmonkeys@gmail.com>
2019-06-09 19:34:08 -0400

    update image names

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,66 +3,74 @@ var woody = {
     attack: 20,
     hp: 120,
     counter: 20,
+    pic:"assets/images/woody.png"
 };
 var buzz = {
     attack: 10,
     hp: 160,
     counter: 10,
+    pic: "assets/images/buzz.png"
 };
 var bullseye = {
     attack: 15,
     hp: 140,
     counter: 15,
+    pic: "assets/images/bullseye.png"
 };
 var jessie = {
     attack: 25,
     hp: 100,
     counter: 25,
+    pic: "assets/images/jessie.png"
 };
 
-function firstClick(){
+var attacker;
+var defender;
+var 
+
+//function firstClick(){
     
 $("#woody").one("click", function(){
     $("#character-one").html(" ");
-    $("#attacker").html('<img alt="woody" id="woody" src="assets/images/kisspng-sheriff-woody-jessie-buzz-lightyear-toy-story-andy-toys-5abd86d623cfd2.7909066115223702621467.png">')
+    $("#attacker").html('<img alt="woody" id="woody" src="assets/images/woody.png">')
     secondClick();
 });
 $("#lightyear").one("click", function(){
     $("#character-two").html(" ");
-    $("#attacker").html('<img alt="buzz" id="lightyear" src="assets/images/kisspng-buzz-lightyear-toy-story-pixar-film-series-toy-story-5abd66b9844b93.6003267515223620415419.png">')
+    $("#attacker").html('<img alt="buzz" id="lightyear" src="assets/images/buzz.png">')
     secondClick();
 });
 $("#bullseye").one("click", function(){
     $("#character-three").html(" ");
-    $("#attacker").html('<img alt="bullseye" id="bullseye" src="assets/images/kisspng-bullseye-jessie-sheriff-woody-horse-buzz-lightyear-toy-story-cartoon-5ae2dcd2bc6395.6827763915248171067717.png">')
+    $("#attacker").html('<img alt="bullseye" id="bullseye" src="assets/images/bullseye.png">')
     secondClick();
 });
 $("#jessie").one("click", function(){
     $("#character-four").html(" ");
-    $("#attacker").html('<img alt="jessie" id="jessie" src="assets/images/kisspng-jessie-toy-story-sheriff-woody-buzz-lightyear-andy-gudi-5ac88f52d7f959.2881777715230933308846.png">')
+    $("#attacker").html('<img alt="jessie" id="jessie" src="assets/images/jessie.png">')
     secondClick();
 });
-};
+//};
 
-firstClick();
+//firstClick();
 
 function secondClick(){
 
 $("#woody").one("click", function(){
     $("#character-one").html(" ");
-    $("#defender").html('<img alt="woody" id="woody" src="assets/images/kisspng-sheriff-woody-jessie-buzz-lightyear-toy-story-andy-toys-5abd86d623cfd2.7909066115223702621467.png">')
+    $("#defender").html('<img alt="woody" id="woody" src="assets/images/woody.png">')
 });
 $("#lightyear").one("click", function(){
     $("#character-two").html(" ");
-    $("#defender").html('<img alt="buzz" id="lightyear" src="assets/images/kisspng-buzz-lightyear-toy-story-pixar-film-series-toy-story-5abd66b9844b93.6003267515223620415419.png">')
+    $("#defender").html('<img alt="buzz" id="lightyear" src="assets/images/buzz.png">')
 });
 $("#bullseye").one("click", function(){
     $("#character-three").html(" ");
-    $("#defender").html('<img alt="bullseye" id="bullseye" src="assets/images/kisspng-bullseye-jessie-sheriff-woody-horse-buzz-lightyear-toy-story-cartoon-5ae2dcd2bc6395.6827763915248171067717.png">')
+    $("#defender").html('<img alt="bullseye" id="bullseye" src="assets/images/bullseye.png">')
 });
 $("#jessie").one("click", function(){
     $("#character-four").html(" ");
-    $("#defender").html('<img alt="jessie" id="jessie" src="assets/images/kisspng-jessie-toy-story-sheriff-woody-buzz-lightyear-andy-gudi-5ac88f52d7f959.2881777715230933308846.png">')
+    $("#defender").html('<img alt="jessie" id="jessie" src="assets/images/jessie.png">')
 });
 
 };
