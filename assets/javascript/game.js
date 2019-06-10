8d6ed93fe349cd78396bffc830554ce8cd6123f1
Greg Smith <thinkable@thinkable.us>
2019-06-09 20:11:55 -0400

    Adding generic click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,33 @@ var woody = {
     attack: 20,
     hp: 120,
     counter: 20,
-    pic: "assets/images/woody.png"
+    pic: "assets/images/woody.png",
+    mydiv: "#character-one",
+    myname: "woody"
 };
 var buzz = {
     attack: 10,
     hp: 160,
     counter: 10,
-    pic: "assets/images/buzz.png"
+    pic: "assets/images/buzz.png",
+    mydiv: "#character-two",
+    myname: "buzz"
 };
 var bullseye = {
     attack: 15,
     hp: 140,
     counter: 15,
-    pic: "assets/images/bullseye.png"
+    pic: "assets/images/bullseye.png",
+    mydiv: "#character-three",
+    myname: "bullseye"
 };
 var jessie = {
     attack: 25,
     hp: 100,
     counter: 25,
-    pic: "assets/images/jessie.png"
+    pic: "assets/images/jessie.png",
+    mydiv: "#character-four",
+    myname: "jessie"
 };
 
 var attacker;
@@ -31,33 +39,23 @@ var where = ["#attacker", "#defender", false];
 var iWhere = 0;
 //function firstClick(){
 
-$("#woody").on("click", function () {
+function firstclick(event) {
+    console.log("event:" + event);
+    var obj = event.data;
     if (where[iWhere]) {
-        $("#character-one").html(" ");
-        $(where[iWhere]).html('<img alt="woody" id="woody" src="assets/images/woody.png">')
+        $(obj.mydiv).html(" ");
+        $(where[iWhere]).html('<img alt="' + obj.myname + '" id="' + '" src="' + obj.pic + '">')
         iWhere++;
     }
-});
-$("#lightyear").on("click", function () {
-    if (where[iWhere]) {
-        $("#character-two").html(" ");
-        $(where[iWhere]).html('<img alt="buzz" id="lightyear" src="assets/images/buzz.png">')
-        iWhere++;
-    }
-});
-$("#bullseye").on("click", function () {
-    if (where[iWhere]) {
-        $("#character-three").html(" ");
-        $(where[iWhere]).html('<img alt="bullseye" id="bullseye" src="assets/images/bullseye.png">')
-        iWhere++;
-    }
-});
-$("#jessie").on("click", function () {
-    if (where[iWhere]) {
-        $("#character-four").html(" ");
-        $(where[iWhere]).html('<img alt="jessie" id="jessie" src="assets/images/jessie.png">')
-        iWhere++;
-    }
-});
+}
+console.log("1");
+$("#woody").on("click", woody, firstclick);
+console.log("2");
+$("#buzz").on("click", buzz, firstclick);
+console.log("3");
+$("#bullseye").on("click", bullseye, firstclick);
+console.log("4");
+$("#jessie").on("click", jessie, firstclick);
+console.log("5");
 
 
